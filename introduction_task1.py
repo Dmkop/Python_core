8f3b5b8868ce01da0814d5a9081dcfdfc49199e7
Dmytro Kopytov <dmytrokopytov@gmail.com>
2019-09-12 21:31:02 +0300

    introduction

---
+++ introduction_task1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.6
+
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+city = input("Where do you live: ")
+
+print("Hello " + name)
+print("Your age is " + str(age))
+print("You live in " + city)
---
+++ introduction_task2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.6
+
+a = int(input("Enter number one: "))
+b = int(input("Enter number two: "))
+
+print("a + b = ", (a + b))
+print("a - b = ", (a - b))
+print("a * b = ", (a * b))
+print("a / b = ", (a / b))
+
+print("a ** b = ", (a ** b))
+print("a // b = ", (a // b))
+print("a % b = ", (a % b))
