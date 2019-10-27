8628c73745871fa421c2a13f21524e17293da3cb
Dmytro Kopytov <dmytrokopytov@gmail.com>
2019-10-28 01:01:03 +0200

    add task_3

---
+++ 01.10_task_3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.6
+
+'''You're re-designing a blog and the blog's posts have the following format for showing the date and time a post was made:
+
+Weekday Month Day, time e.g., Friday May 2, 7pm
+
+You're running out of screen real estate, and on some pages you want to display a shorter format, Weekday Month Day that omits the time.
+
+Write a function, shortenToDate, that takes the Website date/time in its original string format, and returns the shortened format.
+
+Assume shortenToDate's input will always be a string, e.g. "Friday May 2, 7pm". 
+Assume shortenToDate's output will be the shortened string, e.g., "Friday May 2"'''
+
+
+
+
+def short_date(long_date):
+	res = long_date.split(',')
+	return res[0]
+
+print(short_date('Friday May 2, 7pm'))
